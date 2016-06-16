90868169eff4a63bc8fefaf9ebbe7650ef610e48
jesse soldat <jsoldat@hotmail.com>
2016-06-16 20:46:12 +0700

    on click of a button the name of the menu button is sent to the controller

--- ext-modules/menu/menuModule.js
+++ ext-modules/menu/menuModule.js
@@ -2,12 +2,20 @@ angular.module('Menu', [])
 
 
 
-.controller('MenuController', ['$scope', function($scope) {
-  
+.controller('MenuController', ['$scope', '$rootScope', function($scope, $rootScope) {
+
+  	this.setActiveElement = function(el) {
+  		$scope.activeElement = el;
+  	};
+  	this.setRoute = function(route) {
+  		console.log(route);
+  		$rootScope.$broadcast('menu-item-selected-event', {route: route})
+  		};
 }])
 .directive('menu', function() {
   return {
   	transclude: true,
+  	controller: 'MenuController',
   	scope: {
   		
   	},
@@ -24,17 +32,21 @@ angular.module('Menu', [])
   	require: '^menu',
   	scope: {
   		label: '@',
-  		icon: '@'
+  		icon: '@',
+  		route: '@'
   	},
     templateUrl: 'ext-modules/menu/menuItem.html',
-    link: function(scope, e, attr, ctrl) {
-
+    link: function (scope, el, attr, ctrl) {
+            el.on('click', function (evt) {
+                evt.stopPropagation();
+                evt.preventDefault();
+                scope.$apply(function () {
+                    ctrl.setActiveElement(el);
+                    ctrl.setRoute(scope.route);
+                });
+            }); 
     }
     
   };
 })
-
-
-
-
 ;
