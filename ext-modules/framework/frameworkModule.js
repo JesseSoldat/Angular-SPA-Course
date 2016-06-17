de3449356802ebf85581978508d7db02b3b094e5
jesse soldat <jsoldat@hotmail.com>
2016-06-17 13:01:22 +0700

    button only shows when the screen size is less than 768

--- ext-modules/framework/frameworkModule.js
+++ ext-modules/framework/frameworkModule.js
@@ -1,11 +1,34 @@
 angular.module('Framework', ['Menu', 'Dashboard'])
 
-.controller('FrameController', ['$scope', function($scope) {
-      $scope.test = 'test';
+.controller('FrameController', ['$scope', '$window', '$timeout', function($scope, $window, $timeout) {
+
+      $scope.isMenuVisible = true;
+      $scope.isMenuButtonVisible = true;
+
       $scope.$on('menu-item-selected-event', function (evt, data) {
                 console.log(evt);
                 $scope.routeString = data.route;
             });
+
+      $($window).on('resize.Framework', function () {
+       
+        $scope.$apply(function () {
+            checkWidth();
+        });
+      });
+      $scope.$on('$destroy', function () {
+        $($window).off('resize.Framework');
+      });
+
+      var checkWidth = function () {
+        var width = Math.max($($window).width(), $window.innerWidth);
+        // console.log(width);
+        $scope.isMenuVisible = (width > 768);
+        $scope.isMenuButtonVisible = !$scope.isMenuVisible;
+      }
+      $timeout(function () {
+        checkWidth();
+      }, 0);
     
 }])
 .directive('framework', function() {
